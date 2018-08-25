fa8adbb9bc41a44854cba6d054d930b74ddc6d6d
Allan Navarro <allan.nb.cr@gmail.com>
2018-08-24 20:32:18 -0600

    se programa metodo de secante, la raiz se calcula adecuadamente, sin embargo lanza un error 201

--- include/RootSecant.hpp
+++ include/RootSecant.hpp
@@ -18,27 +18,37 @@
 #define ANPI_ROOT_SECANT_HPP
 
 namespace anpi {
-  
-  /**
-   * Find a root of the function funct looking for it starting at xi
-   * by means of the secant method.
-   *
-   * @param funct a functor of the form "T funct(T x)"
-   * @param xi initial position
-   * @param xii second initial position 
-   *
-   * @return root found, or NaN if no root could be found
-   */
-  template<typename T>
-  T rootSecant(const std::function<T(T)>& funct,T xi,T xii,const T eps) {
 
-    // TODO: Put your code in here!
-    
-    // Return NaN if no root was found
-    return std::numeric_limits<T>::quiet_NaN();
-  }
+    /**
+     * Find a root of the function funct looking for it starting at xi
+     * by means of the secant method.
+     *
+     * @param funct a functor of the form "T funct(T x)"
+     * @param xi initial position
+     * @param xii second initial position
+     *
+     * @return root found, or NaN if no root could be found
+     */
+    template<typename T>
+    T rootSecant(const std::function<T(T)> &funct, T xi, T xii, const T eps) {
+
+        int maxi = 10000;
+        T dx;
+        T tmp;
+        for (int i = 0; i < maxi; ++i) {
+            tmp = xii;
+            xii = xii - funct(xii) * (xi - xii) / (funct(xi) - funct(xii));
+            xi = tmp;
+            dx = std::abs(xii-xi);
+            if (dx < eps){
+                return std::abs(funct(xii))<=eps? xii : std::numeric_limits<T>::quiet_NaN();
+            }
+        }
+        // Return NaN if no root was found
+        return std::numeric_limits<T>::quiet_NaN();
+    }
 
 }
-  
+
 #endif
 
