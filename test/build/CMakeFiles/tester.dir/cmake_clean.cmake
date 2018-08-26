b4bcd2add94f5bc39c3c743bb2540200d31047f7
chris <chrisarrefall@gmail.com>
2018-08-25 20:25:31 -0600

    Se arreglo bug del testRootFinders

---
+++ test/build/CMakeFiles/tester.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tester.dir/testAllocator.o"
+  "CMakeFiles/tester.dir/testMain.o"
+  "CMakeFiles/tester.dir/testMatrix.o"
+  "CMakeFiles/tester.dir/testRootFinders.o"
+  "tester.pdb"
+  "tester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
