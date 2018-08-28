d437365873122ad6dd14d43816e56d227d0d9056
Allan Navarro <allan.nb.cr@gmail.com>
2018-08-28 08:06:17 -0600

    correcciones

--- include/RootBisection.hpp
+++ include/RootBisection.hpp
@@ -34,37 +34,40 @@ namespace anpi {
     template<typename T>
     T rootBisection(const std::function<T(T)> &funct, T xl, T xu, const T eps) {
 
-        if (xl > xu) {
+        if (xl >= xu) {//check for correct interval
             throw anpi::Exception("inverted interval");
         }
-        if (funct(xl) * funct(xu) > T(0)) {
+        if (funct(xl) * funct(xu) > T(0)) {//check if both extremes have same sign
             throw anpi::Exception("unenclosed root");
         }
 
         int maxi = 10000;
-        T xr = xl;
+        T xr = xl; // estimate the root as the lower limit
         T fl = funct(xl);
-        T ea = T();
+        T ea = T(); //relative error
         for (int i = 0; i < maxi; ++i) {
             T xrold(xr);
             xr = (xl + xu) / T(2);
             T fr = funct(xr);
 
-            if (std::abs(xr) > std::numeric_limits<T>::epsilon()) {
-                ea = std::abs((xrold - xr) / xr) * T(100);
+            if (std::abs(xr) > std::numeric_limits<T>::epsilon()) {//check division for 0
+                ea = std::abs((xrold - xr) / xr) * T(100); // calculate the approximate relative error
             }
             T cond = fl * fr;
-            if (cond < T(0)) {
+            if (cond < T(0)) {//continuew with left side
                 xu = xr;
-            } else if (cond > T(0)) {
+            } else if (cond > T(0)) {//continuew with right side
                 xl = xr;
                 fl = fr;
-            } else {
+            } else {// there was no error!
                 ea = T(0);
                 xr = (std::abs(fl) < std::numeric_limits<T>::epsilon()) ? xl : xr;
             }
-            if (ea < eps) return xr;
+            if (ea < eps) {
+                return xr;
+            }
         }
+        // Return NaN if no root was found
         return std::numeric_limits<T>::quiet_NaN();
     }
 }
--- include/RootBrent.hpp
+++ include/RootBrent.hpp
@@ -18,29 +18,97 @@
 #define ANPI_ROOT_BRENT_HPP
 
 namespace anpi {
-  
-  /**
-   * Find the roots of the function funct looking for it in the
-   * interval [xl,xu], using the Brent's method.
-   *
-   * @param funct a std::function of the form "T funct(T x)"
-   * @param xl lower interval limit
-   * @param xu upper interval limit
-   *
-   * @return root found, or NaN if none could be found.
-   *
-   * @throws anpi::Exception if inteval is reversed or both extremes
-   *         have same sign.
-   */
-  template<typename T>
-  T rootBrent(const std::function<T(T)>& funct,T xl,T xu,const T eps) {
 
-    // TODO: Put your code in here!
+    /**
+     * Find the roots of the function funct looking for it in the
+     * interval [xl,xu], using the Brent's method.
+     *
+     * @param funct a std::function of the form "T funct(T x)"
+     * @param xl lower interval limit
+     * @param xu upper interval limit
+     *
+     * @return root found, or NaN if none could be found.
+     *
+     * @throws anpi::Exception if inteval is reversed or both extremes
+     *         have same sign.
+     */
+    template<typename T>
+    T rootBrent(const std::function<T(T)> &funct, T xl, T xu, const T eps) {
 
-    // Return NaN if no root was found
-    return std::numeric_limits<T>::quiet_NaN();
-  }
+        if (xl >= xu) {//check for correct interval
+            throw anpi::Exception("inverted interval");
+        }
+        if (funct(xl) * funct(xu) > T(0)) {//check if both extremes have same sign
+            throw anpi::Exception("unenclosed root");
+        }
+
+        auto maxi = 10000;
+        auto eps1 = std::numeric_limits<T>::epsilon();
+
+        T a = xl, b = xu, c = xu, d, e, fa = funct(a), fb = funct(b), fc, p, q, r, s, tol1, xm;
+        fc = fb;
+        for (int i = maxi; i >= 0; --i) {//start the loop
+            if ((fb > T(0) && fc > T(0)) || (fb < T(0) && fc < T(0))) {
+                c = a;
+                fc = fa;
+                e = d = b - a;
+            }
+            if (std::abs(fc) < std::abs(fb)) {
+                a = b;
+                b = c;
+                c = a;
+                fa = fb;
+                fb = fc;
+                fc = fa;
+            }
+            tol1 = T(2) * eps1 * std::abs(b) + 0.5 * eps;//convergence check
+            xm = 0.5 * (c - b);
+            if (std::abs(xm) <= tol1 || fb == T(0)) {
+                return b;
+            }
+            if (std::abs(e) >= tol1 && std::abs(fa) > std::abs(fb)) {
+                s = fb / fa;  //Attempt inverse quadratic interpolation
+                if ((a-c)<eps1) {
+                    p = T(2) * xm * s;
+                    q = T(1) - s;
+                } else {
+                    q = fa / fc;
+                    r = fb / fc;
+                    p = s * (T(2) * xm * q * (q - r) - (b - a) * (r - T(1)));
+                    q = (q - T(1)) * (r - T(1)) * (s - T(1));
+                }
+                if (p > T(0)) {
+                    q = -q; //Check whether in bounds
+                }
+                p = std::abs(p);
+                T min1 = T(3) * xm * q - std::abs(tol1 * q);
+                T min2 = std::abs(e * q);
+                if (T(2) * q < (min1 < min2 ? min1 : min2)) {
+                    e = d; // accept interpolation
+                    d = p / q;
+                } else {
+                    d = xm; //interpolation failed
+                    e = d;
+                }
+            } else { //bounds decreasing too slowly
+                d = xm;
+                e = d;
+            }
+            a = b;
+            fa = fb;
+            if (std::abs(d) > tol1){ //evaluate new trial root
+                b += d;
+            }
+            else
+            {
+                b += (((xm) >= 0.0) ? std::abs(tol1) : -std::abs(tol1));
+            }
+            fb = funct(b);
+        }
+
+        // Return NaN if no root was found
+        return std::numeric_limits<T>::quiet_NaN();
+    }
 }
-  
 #endif
 
